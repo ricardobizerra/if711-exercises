edd489f3e452ddd53577c5e469074a792d9239db
ricardobizerra <ricardo.bizerra19@gmail.com>
2025-03-11 23:39:24 -0300

    feat: duplication of matrix service for rabbitmq implementation

---
+++ exercicio-06-djaar-rblf/rabbitmq/matrix.go
@@ -0,0 +1,16 @@
+package rabbitmq
+
+import (
+	"exercicio-06-djaar-rblf/matrix"
+	"exercicio-06-djaar-rblf/shared"
+)
+
+type MatrixService struct{}
+
+func (s *MatrixService) Multiply(req shared.Request, res *shared.Reply) error {
+	response := matrix.Multiply(req.A, req.B)
+
+	res.R = response
+
+	return nil
+}
